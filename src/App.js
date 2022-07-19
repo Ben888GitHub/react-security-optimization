fa00165bb29c90a513f4629268cd1b65f62ea731
Benedict <benedictryan80@gmail.com>
2022-07-19 23:28:10 +0700

    implement parse url in react router dom Link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import CorsExample from './components/CorsExample';
+import ValidatedURL from './components/ValidatedURL';
 
 function App() {
+	const validateUrl = (url) => {
+		const parsed = new URL(url, document.referrer);
+		console.log(url);
+		console.log(document.referrer);
+		console.log(parsed);
+		return ['https:', 'http:'].includes(parsed.protocol);
+	};
+
 	return (
 		<div className="App">
 			<h1>Security Optimization React</h1>
-			<br />
-			<CorsExample />
+			<Routes>
+				<Route path="/" element={<CorsExample />} />
+				<Route path="/validatedurl" element={<ValidatedURL />} />
+			</Routes>
+			<h2>
+				<Link to={validateUrl('validatedurl') && 'validatedurl'}>
+					Validate URL
+				</Link>
+			</h2>
 		</div>
 	);
 }
---
+++ src/components/ValidatedURL.js
@@ -0,0 +1,5 @@
+function ValidatedURL() {
+	return <h1>ValidatedURL</h1>;
+}
+
+export default ValidatedURL;
