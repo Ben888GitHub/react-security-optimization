870f037ea2db1364439e6a684da0324b47e3d22c
Benedict <benedictryan80@gmail.com>
2022-07-19 23:33:11 +0700

    opt out url parsing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import CorsExample from './components/CorsExample';
 import ValidatedURL from './components/ValidatedURL';
 
 function App() {
-	const validateUrl = (url) => {
-		const parsed = new URL(url, document.referrer);
-		console.log(url);
-		console.log(document.referrer);
-		console.log(parsed);
-		return ['https:', 'http:'].includes(parsed.protocol);
-	};
-
 	return (
 		<div className="App">
 			<h1>Security Optimization React</h1>
@@ -19,11 +11,6 @@ function App() {
 				<Route path="/" element={<CorsExample />} />
 				<Route path="/validatedurl" element={<ValidatedURL />} />
 			</Routes>
-			<h2>
-				<Link to={validateUrl('validatedurl') && 'validatedurl'}>
-					Validate URL
-				</Link>
-			</h2>
 		</div>
 	);
 }
